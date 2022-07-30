fd7a48ac7605e4bab2a48914648a2c6eb0be31e3
Matheus <matheus_sousa@protonmail.com>
2022-07-29 23:40:37 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from '@chakra-ui/react'
+
+function App() {
+  
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          "The quick brown fox jumps over the lazy dog" is an English-language pangram—a
+          sentence that contains all of the letters of the English alphabet. Owing to
+          its existence, Chakra was created.
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default App
